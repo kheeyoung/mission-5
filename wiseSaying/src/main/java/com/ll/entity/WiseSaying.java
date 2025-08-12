927abeebef1e4e5289904629fc9071dd7ebf5b95
kheeyoung <talent8661@gmail.com>
2025-08-12 12:18:18 +0900

    롬복 추가!

--- wiseSaying/src/main/java/com/ll/entity/WiseSaying.java
+++ wiseSaying/src/main/java/com/ll/entity/WiseSaying.java
@@ -1,33 +1,14 @@
 package com.ll.entity;
 
+import jdk.jfr.Enabled;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class WiseSaying {
     private int id;
     private String quote;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getQuote() {
-        return quote;
-    }
-
-    public void setQuote(String quote) {
-        this.quote = quote;
-    }
-
-    public String getWriter() {
-        return writer;
-    }
-
-    public void setWriter(String writer) {
-        this.writer = writer;
-    }
-
     private String writer;
 
     public WiseSaying(int id, String quote, String writer) {
